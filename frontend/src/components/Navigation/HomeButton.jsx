d65bd0bf0a2aa2c91caefbb70d60e0cb2c09028b
Yusuf Kara <yusufkara5562@gmail.com>
2024-11-26 23:48:52 +0300

    update css

--- frontend/src/components/Navigation/HomeButton.jsx
+++ frontend/src/components/Navigation/HomeButton.jsx
@@ -1,10 +1,18 @@
 import { CiHome } from "react-icons/ci";
 import './Navigation.css'
 
+// const HomeButton = () => {
+//   return (
+//     <div>
+//       <CiHome className="nav-icon" title="Home" />
+//     </div>
+//   )
+// };
+
 const HomeButton = () => {
   return (
-    <div>
-      <CiHome className="nav-icon" title="Home" />
+    <div id="home-link">
+      <img id="home-logo" src="/homebnb-logo-transparent.png" title="Home" alt="homebnb-logo" />
     </div>
   )
 };
